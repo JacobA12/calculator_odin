982bfb51e050411cd343618eecf8de6bdf9915ba
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-04-30 17:08:32 -0700

    base

---
+++ script.js
@@ -0,0 +1,22 @@
+let numA = 0;
+let numB = 0;
+
+function add(numA, numB){
+  return numA + numB;
+}
+
+function subtract(numA, numB){
+  return numA - numB;
+}
+
+function multiply(numA, numB){
+  return numA * numB;
+}
+
+function divide(numA, numB){
+  return numA / numB;
+}
+
+function operate(operator, numA, numB){
+  
+}
