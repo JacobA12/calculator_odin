efe3bf90ddc0e851eb7d297f3648e62a7072b9c3
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-05-06 15:25:57 -0700

    added button variables

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-
 let numA = 0;
 let numB = 0;
 
+const numberButtons = document.querySelectorAll("[data-number]");
+const opeartorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equals-button");
+const pointButton = document.getElementById("point-button");
+const deleteButton = document.getElementById("del-button");
+const clearButton = document.getElementById("clear-button");
 
-function add(numA, numB){
+function add(numA, numB) {
   return numA + numB;
 }
 
-function subtract(numA, numB){
+function subtract(numA, numB) {
   return numA - numB;
 }
 
-function multiply(numA, numB){
+function multiply(numA, numB) {
   return numA * numB;
 }
 
-function divide(numA, numB){
+function divide(numA, numB) {
   return numA / numB;
 }
-
